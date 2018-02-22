c79ca41420c457f83582aa2cb32d1ff5b6547639
jmisiti <jmisiti42@gmail.com>
2018-02-22 11:12:24 +0100

    Modification

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,72 @@
+const mongoose = require('mongoose');
+require("../models/User");
+const User = mongoose.model('User');
+
 const UserController = function () {
-	this.checkAuth = (req, res, next) => {
-		if (!req.session || !req.session.authenticated) {
-			res.send('oops');
+	this.isLoggedIn = (req, res, next) => {
+		if (!req.session) {
+			res.send('You need to login before access this page !');
 		} else {
-			res.send('gg');
+			res.send('Welcome !');
 		}
 		next();
-	}
+	};
+
+	this.signup = (req, res, next) => {
+		if (req.session) {
+			res.send('Already logged in !');
+		} else if (req.body) {
+			let errors = verifyFields(req);
+			if (errors.length > 0) {
+				res.json({ "errors": errors });
+			} else {
+				let user = new User();
+				user = user.create(req.body);
+				console.log(user);
+				res.send("account created !");
+			}
+		} else {
+			res.send('An error occured');
+		}
+		next();
+	};
+
+	this.signin = (req, res, next) => {
+		if (req.session) {
+			res.send('Already logged in !');
+		} else if (req.body) {
+			if (req.body.password && req.body.mail && validateEmail(req.body.mail)) {
+
+			}
+		} else {
+			res.send('An error occured');
+		}
+		next();
+	};
 }
 
 module.exports = new UserController();
+
+const validateEmail = (email) => {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
+const verifyFields = (req) => {
+	let errors = new Array();
+	if (!req.body.mail || !validateEmail(req.body.mail))
+		errors.push('Veuillez entrer une addresse e-mail valide.');
+	if (!req.body.password)
+		errors.push('Veuillez renseigner votre mot de passe.');
+	if (!req.body.repassword)
+		errors.push('Veuillez confirmer votre mot de passe.');
+	if (req.body.password != req.body.repassword)
+		errors.push('Vos mots de passes ne correspondent pas.');
+	if (req.body.childs && req.body.childs.length > 0) {
+		for (var i = 0; i != req.body.childs.length; i++) {
+			if (req.body.childs[i].length <= 0 || isNaN(req.body.childs[i]))
+				errors.push('Enfant ' + i + " mal formaté");
+		}
+	}
+	return errors;
+}
--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+const mongoose 				= require('mongoose');
 const express				= require('express');
 const bodyParser			= require('body-parser');
 const http					= require('http');
 const config				= require('./config/production.json');
 const userController		= require('./controller/userController');
+const fs 					= require('fs');
+const path					= require('path');
 const app					= express();
-const validateEmail			= (email) => {
-	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(email);
-}
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(require('helmet')());
+mongoose.connect(config.uri);
 
 app.set('view engine', 'ejs');
 app.disable('x-powered-by');
 app.enable('trust proxy');
 
-app.get('/', userController.checkAuth);
+app.get('/audio', function (req, res) {
+	res.render("index");
+});
+
+
+app.get('/', userController.isLoggedIn);
+
+app.post('/signup', userController.signup);
+app.post('/signin', userController.signin);
 
 http.createServer(app).listen(5001, () => console.log(`listening on port 5001`));
